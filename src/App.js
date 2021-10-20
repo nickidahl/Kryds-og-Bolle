204a166e1609afd6a24b3d5f99336a5e3d9e08e2
Nicki Ortmann Dahl <6417280@RTS-365.dk>
2021-10-20 21:12:36 +0200

    spillet er done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hey abigail</h1>
+        <Game />
       </header>
     </div>
   );
